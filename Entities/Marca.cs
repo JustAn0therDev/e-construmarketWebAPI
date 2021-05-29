3081cfb2871ad1217525dc0113b3ba29d16f79cf
Ruan Montelo <ruanmontelo@gmail.com>
2021-05-29 14:37:38 -0300

    Adicionando teste para rota de /produto e mais itens no JSON de produtos

--- Entities/Marca.cs
+++ Entities/Marca.cs
@@ -1,5 +1,8 @@
 namespace Entities
 {
+    /// <summary>
+    /// Classe que representa uma Marca com código
+    /// </summary>
     public class Marca
     {
         public string Nome { get; set; }
---
+++ Tests/ControllerTests/ProdutoControllerTests.cs
@@ -0,0 +1,43 @@
+﻿using DAO.Interfaces;
+using e_construmarket.Controllers;
+using Entities;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Tests.ControllerTests
+{
+    public class ProdutoControllerTests
+    {
+        [Fact]
+        public void GetShouldReturnEnumerableOfProductsWithStatusCode200()
+        {
+            ILoggerFactory loggerFactory = LoggerFactory.Create(action => action.AddDebug());
+            var logger = new Logger<ProdutoController>(loggerFactory);
+
+            var mock = new Mock<IProdutoDAO>();
+
+            mock.Setup(s => s.GetAll()).Returns(new List<Produto>
+            {
+                new Produto 
+                {
+                    Nome = "Teclado", 
+                    Preco = 99.9M, 
+                    Marca = new Marca { Nome = "Razer", CodigoMarca = Guid.NewGuid().ToString() }
+                }
+            });
+
+            IProdutoDAO produtoDAO = mock.Object;
+
+            ProdutoController controller = new ProdutoController(produtoDAO, logger);
+            var result = controller.Get();
+
+            var controllerResponseList = result.Value as List<Produto>;
+
+            Assert.True(result.StatusCode == 200);
+            Assert.True(controllerResponseList.Count > 0);
+        }
+    }
+}
